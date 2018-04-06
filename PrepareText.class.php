615c13816e7301e7e40d8f06e1a8ddd907a3950c
Marcelo Félix <marcelofelix.af@gmail.com>
2018-04-06 14:03:40 -0300

    add funtions

--- PrepareText.class.php
+++ PrepareText.class.php
@@ -1,17 +1,26 @@
 <?php
-
 	Class PrepareText {
-
-		var $text;
+		var $txt_prep;
+		var $words = array("galo","doido","cabeca","quatro","fala","palavras","aluno","rumo","braco","dois","pe","rei","roma","roeu","atletico","seis","ipatinga","jota","menisco","frangas","tremeram","mineirao","bigode","proerd","camisa","quarto","quadro");
+		var $remove_words = array("e","ou","o","a","os","as","ao","aos","do","da","dos","das","no","na","nos","nas","pelo","pela","pelos","pelas","um","uma","uns","umas","dum","duma","duns","dumas","num","numa","nuns","numas", "a","ante","apos","ate","com","conforme","contra","consoante","de","desde","durante","em","excepto","entre","mediante","para","perante","por","salvo","sem","segundo","sob","sobre","tras");
+		var $result = array();
 
 		function __construct($text){
-			$this -> text = $text;
-			echo $text;
+			$this -> txt_prep = $text;
+			$this -> all__preparations();
 		}
 
-		function imprimir(){
-			echo "asdasd";
+		function all__preparations() {
+			$this -> txt_prep = explode(' ', $this -> txt_prep);
+			$this -> txt_prep = array_diff($this -> txt_prep, $this -> remove_words);
+			$this -> txt_prep = implode(' ', $this -> txt_prep);
+			for($i = 0; $i < count($this -> words); $i++){
+	            $this -> result[$i] = substr_count($this -> txt_prep, $this -> words[$i]);
+         	}
+		} 
+			
+		function return__vector() {
+			return $this -> result;
 		}
 	}
-
 ?>
--- bag_of_words.php
+++ bag_of_words.php
@@ -1,17 +1,38 @@
 <?php
 	require_once 'PrepareText.class.php'; 
-	$remove_words = array("e","ou","o","a","os","as","ao","aos","do","da","dos","das","no","na","nos","nas","pelo","pela","pelos","pelas","um","uma","uns","umas","dum","duma","duns","dumas","num","numa","nuns","numas", "a","ante","apos","ate","com","conforme","contra","consoante","de","desde","durante","em","excepto","entre","mediante","para","perante","por","salvo","sem","segundo","sob","sobre","tras");
-	$words = array("galo","doido","cabeca","quatro","fala","palavras","aluno","rumo","braco","dois","pe","rei","roma","roeu","atletico","seis","ipatinga","jota","menisco","frangas","tremeram","mineirao","bigode","proerd","camisa","quarto","quadro");
-	
-	
-	read("docA", "docB");
+	$result_doc1; $result_doc2;	
 
-	function read($doc1, $doc2) {
+	function read__and__preparation($doc1, $doc2) {
         $fp = fopen($doc1.".txt", "r"); 
         $pt = new PrepareText(fread($fp, filesize($doc1.".txt")));
+        global $result_doc1;
+       	$result_doc1 = $pt -> return__vector();
         fclose($fp);
         $fp = fopen($doc2.".txt", "r"); 
         $pt = new PrepareText(fread($fp, filesize($doc2.".txt")));
+        global $result_doc2;
+       	$result_doc2 = $pt -> return__vector();
         fclose($fp);
 	}
+
+	function calculation($vector1, $vector2) {
+		$vector_calculations = array(0, 0, 0);
+		for($i = 0; $i < count($vector1); $i++){
+            $vector_calculations[0] += ($vector1[$i]*$vector2[$i]);
+        }  
+        for($i = 0; $i < count($vector1); $i++){
+            $vector_calculations[1] += ($vector1[$i]*$vector1[$i]);
+            $vector_calculations[2] += ($vector2[$i]*$vector2[$i]);
+        }
+        if((sqrt($vector_calculations[1]) * sqrt($vector_calculations[2])) == 0) {
+        	var_dump("Resultado = 0");
+        }else{
+        	var_dump("Resultado = ".$vector_calculations[0] / (sqrt($vector_calculations[1]) * sqrt($vector_calculations[2])));
+        }
+	}
+	
+	read__and__preparation("docA", "docB");
+	var_dump($result_doc1);
+	var_dump($result_doc2);
+	calculation($result_doc1,$result_doc2);
 ?>

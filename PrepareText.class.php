175f7d0b4863715454cc1871f1d8dcb4b36815a4
Marcelo Félix <marcelofelix.af@gmail.com>
2018-04-11 10:49:13 -0300

    100%

--- PrepareText.class.php
+++ PrepareText.class.php
@@ -1,22 +1,33 @@
 <?php
 	Class PrepareText {
 		var $txt_prep;
+		var $type;
 		var $words = array("galo","doido","cabeca","quatro","fala","palavras","aluno","rumo","braco","dois","pe","rei","roma","roeu","atletico","seis","ipatinga","jota","menisco","frangas","tremeram","mineirao","bigode","proerd","camisa","quarto","quadro");
 		var $remove_words = array("e","ou","o","a","os","as","ao","aos","do","da","dos","das","no","na","nos","nas","pelo","pela","pelos","pelas","um","uma","uns","umas","dum","duma","duns","dumas","num","numa","nuns","numas", "a","ante","apos","ate","com","conforme","contra","consoante","de","desde","durante","em","excepto","entre","mediante","para","perante","por","salvo","sem","segundo","sob","sobre","tras");
 		var $result = array();
 
-		function __construct($text){
+		function __construct($text, $type){
 			$this -> txt_prep = $text;
+			$this -> type = $type;
 			$this -> all__preparations();
 		}
 
 		function all__preparations() {
 			$this -> txt_prep = explode(' ', $this -> txt_prep);
 			$this -> txt_prep = array_diff($this -> txt_prep, $this -> remove_words);
-			$this -> txt_prep = implode(' ', $this -> txt_prep);
-			for($i = 0; $i < count($this -> words); $i++){
-	            $this -> result[$i] = substr_count($this -> txt_prep, $this -> words[$i]);
-         	}
+			if($this -> type == "binary") {
+				$this -> txt_prep = array_unique($this -> txt_prep);
+				sort($this -> txt_prep);
+				$this -> txt_prep = implode(' ', $this -> txt_prep);
+				for($i = 0; $i < count($this -> words); $i++){
+		            $this -> result[$i] = substr_count($this -> txt_prep, $this -> words[$i]);
+	         	}
+			}else {
+				$this -> txt_prep = implode(' ', $this -> txt_prep);
+				for($i = 0; $i < count($this -> words); $i++){
+		            $this -> result[$i] = substr_count($this -> txt_prep, $this -> words[$i]);
+	         	}
+			}
 		} 
 			
 		function return__vector() {
--- bag_of_words.php
+++ bag_of_words.php
@@ -1,21 +1,22 @@
 <?php
 	require_once 'PrepareText.class.php'; 
-	$result_doc1; $result_doc2;	
+	$result_doc1; $result_doc2;
 
-	function read__and__preparation($doc1, $doc2) {
-        $fp = fopen($doc1.".txt", "r"); 
-        $pt = new PrepareText(fread($fp, filesize($doc1.".txt")));
+
+	function read__and__preparation($doc1, $doc2, $type) {
+        $fp = fopen("docs/".$doc1.".txt", "r"); 
+        $pt = new PrepareText(fread($fp, filesize("docs/".$doc1.".txt")), $type);
         global $result_doc1;
        	$result_doc1 = $pt -> return__vector();
         fclose($fp);
-        $fp = fopen($doc2.".txt", "r"); 
-        $pt = new PrepareText(fread($fp, filesize($doc2.".txt")));
+        $fp = fopen("docs/".$doc2.".txt", "r"); 
+        $pt = new PrepareText(fread($fp, filesize("docs/".$doc2.".txt")), $type);
         global $result_doc2;
        	$result_doc2 = $pt -> return__vector();
         fclose($fp);
 	}
 
-	function calculation($vector1, $vector2) {
+	function calculation($vector1, $vector2, $text) {
 		$vector_calculations = array(0, 0, 0);
 		for($i = 0; $i < count($vector1); $i++){
             $vector_calculations[0] += ($vector1[$i]*$vector2[$i]);
@@ -24,15 +25,21 @@
             $vector_calculations[1] += ($vector1[$i]*$vector1[$i]);
             $vector_calculations[2] += ($vector2[$i]*$vector2[$i]);
         }
-        if((sqrt($vector_calculations[1]) * sqrt($vector_calculations[2])) == 0) {
-        	var_dump("Resultado = 0");
-        }else{
-        	var_dump("Resultado = ".$vector_calculations[0] / (sqrt($vector_calculations[1]) * sqrt($vector_calculations[2])));
-        }
+        var_dump($text." = ".$vector_calculations[0] / (sqrt($vector_calculations[1]) * sqrt($vector_calculations[2])));   
 	}
-	
-	read__and__preparation("docA", "docB");
-	var_dump($result_doc1);
-	var_dump($result_doc2);
-	calculation($result_doc1,$result_doc2);
+
+    //read__and__preparation("doc1", "doc6");
+    //calculation($result_doc1,$result_doc2, "Similaridade ");
+
+    for($i = 1; $i < 10; $i++) {
+        for($y = 1; $y < 10; $y++) {
+            if($y != $i) {
+                read__and__preparation("doc".$i, "doc".$y, "frequencia");
+                calculation($result_doc1,$result_doc2, "Frequencia - Similaridade entre arquivo Doc".$i." e Doc".$y);
+                read__and__preparation("doc".$i, "doc".$y, "binary");
+                calculation($result_doc1,$result_doc2, "Binary - Similaridade entre arquivo Doc".$i." e Doc".$y);
+                echo "------------------------------------";
+            }
+        }
+    }
 ?>
